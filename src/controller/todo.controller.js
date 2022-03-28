cbce99151b0a1975907118f695c0e3af122eb8a3
fahadalim <mohammadfahadalim@gmail.com>
2022-03-28 10:55:26 +0530

    c4

--- src/controller/todo.controller.js
+++ src/controller/todo.controller.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const router = express.Router
+const router = express.Router()
 const authenticate = require("../middleware/authenticate");
 const Todo = require("../models/todo.model")
 
